37122052266cd1ca1716a90c3bb6f15494b5c820
lbischof <lorenz.bischof@sbb.ch>
2018-11-13 12:40:34 +0100

    Update size format error for NFS

--- server/openshift/volume.go
+++ server/openshift/volume.go
@@ -24,6 +24,7 @@ import (
 
 const (
 	wrongSizeFormatError  = "Ungültige Grösse. Format muss Zahl gefolgt von M/G sein (z.B. 500M)."
+	wrongSizeNFSFormatError  = "Ungültige Grösse. Format muss Zahl gefolgt von G sein (z.B. 1G)."
 	wrongSizeLimitError   = "Grösse nicht erlaubt. Mindestgrösse: 500M (1G für NFS). Maximale Grössen sind: M: %v, G: %v"
 	apiCreateWorkflowUuid = "64b3b95b-0d79-4563-8b88-f8c4486b40a0"
 	apiChangeWorkflowUuid = "186b1295-1b82-42e4-b04d-477da967e1d4"
@@ -196,7 +197,7 @@ func validateSizeFormat(size string, technology string) error {
 		if strings.HasSuffix(size, "G") {
 			return nil
 		}
-		return errors.New(wrongSizeFormatError)
+		return errors.New(wrongSizeNFSFormatError)
 	}
 	if strings.HasSuffix(size, "M") || strings.HasSuffix(size, "G") {
 		return nil
