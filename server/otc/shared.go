e080782e123365eeaf4b6f6a7a1273030a0a96ae
lbischof <1837725+lbischof@users.noreply.github.com>
2019-02-19 12:46:04 +0100

    Use updated function

--- server/otc/shared.go
+++ server/otc/shared.go
@@ -34,7 +34,7 @@ var (
 )
 var nilOptions = gophercloud.AuthOptions{}
 
-// From https://github.com/rackspace/gophercloud/blob/master/openstack/auth_env.go
+// From https://github.com/gophercloud/gophercloud/blob/master/openstack/auth_env.go
 // Updated to use viper and fallback to env
 func authOptionsFromEnv() (gophercloud.AuthOptions, error) {
 	cfg := config.Config()
@@ -47,32 +47,78 @@ func authOptionsFromEnv() (gophercloud.AuthOptions, error) {
 	tenantName := cfg.GetString("os_tenant_name")
 	domainID := cfg.GetString("os_domain_id")
 	domainName := cfg.GetString("os_domain_name")
-	tokenID := cfg.GetString("os_token")
+	applicationCredentialID := cfg.GetString("os_application_credential_id")
+	applicationCredentialName := cfg.GetString("os_application_credential_name")
+	applicationCredentialSecret := cfg.GetString("os_application_credential_secret")
+
+	// If OS_PROJECT_ID is set, overwrite tenantID with the value.
+	if v := cfg.GetString("os_project_id"); v != "" {
+		tenantID = v
+	}
+
+	// If OS_PROJECT_NAME is set, overwrite tenantName with the value.
+	if v := cfg.GetString("os_project_name"); v != "" {
+		tenantName = v
+	}
 
 	// end custom part
 
 	if authURL == "" {
-		return nilOptions, ErrNoAuthURL
+		err := gophercloud.ErrMissingEnvironmentVariable{
+			EnvironmentVariable: "OS_AUTH_URL",
+		}
+		return nilOptions, err
 	}
 
-	if username == "" && userID == "" && tokenID == "" {
-		return nilOptions, ErrNoUsername
+	if userID == "" && username == "" {
+		// Empty username and userID could be ignored, when applicationCredentialID and applicationCredentialSecret are set
+		if applicationCredentialID == "" && applicationCredentialSecret == "" {
+			err := gophercloud.ErrMissingAnyoneOfEnvironmentVariables{
+				EnvironmentVariables: []string{"OS_USERID", "OS_USERNAME"},
+			}
+			return nilOptions, err
+		}
 	}
 
-	if password == "" && tokenID == "" {
-		return nilOptions, ErrNoPassword
+	if password == "" && applicationCredentialID == "" && applicationCredentialName == "" {
+		err := gophercloud.ErrMissingEnvironmentVariable{
+			EnvironmentVariable: "OS_PASSWORD",
+		}
+		return nilOptions, err
+	}
+
+	if (applicationCredentialID != "" || applicationCredentialName != "") && applicationCredentialSecret == "" {
+		err := gophercloud.ErrMissingEnvironmentVariable{
+			EnvironmentVariable: "OS_APPLICATION_CREDENTIAL_SECRET",
+		}
+		return nilOptions, err
+	}
+
+	if applicationCredentialID == "" && applicationCredentialName != "" && applicationCredentialSecret != "" {
+		if userID == "" && username == "" {
+			return nilOptions, gophercloud.ErrMissingAnyoneOfEnvironmentVariables{
+				EnvironmentVariables: []string{"OS_USERID", "OS_USERNAME"},
+			}
+		}
+		if username != "" && domainID == "" && domainName == "" {
+			return nilOptions, gophercloud.ErrMissingAnyoneOfEnvironmentVariables{
+				EnvironmentVariables: []string{"OS_DOMAIN_ID", "OS_DOMAIN_NAME"},
+			}
+		}
 	}
 
 	ao := gophercloud.AuthOptions{
-		IdentityEndpoint: authURL,
-		UserID:           userID,
-		Username:         username,
-		Password:         password,
-		TenantID:         tenantID,
-		TenantName:       tenantName,
-		DomainID:         domainID,
-		DomainName:       domainName,
-		TokenID:          tokenID,
+		IdentityEndpoint:            authURL,
+		UserID:                      userID,
+		Username:                    username,
+		Password:                    password,
+		TenantID:                    tenantID,
+		TenantName:                  tenantName,
+		DomainID:                    domainID,
+		DomainName:                  domainName,
+		ApplicationCredentialID:     applicationCredentialID,
+		ApplicationCredentialName:   applicationCredentialName,
+		ApplicationCredentialSecret: applicationCredentialSecret,
 	}
 
 	return ao, nil
