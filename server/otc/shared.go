60447b2b97896096e085c99b668af31f9c283e1d
lbischof <1837725+lbischof@users.noreply.github.com>
2019-02-19 13:00:34 +0100

    Use separate file

--- server/otc/shared.go
+++ server/otc/shared.go
@@ -3,7 +3,6 @@ package otc
 import (
 	"errors"
 	"fmt"
-	"github.com/SchweizerischeBundesbahnen/ssp-backend/server/config"
 	"github.com/gin-gonic/gin"
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
@@ -25,103 +24,6 @@ func RegisterRoutes(r *gin.RouterGroup) {
 	r.GET("/otc/images", listImagesHandler)
 	r.GET("/otc/availabilityzones", listAvailabilityZonesHandler)
 	r.GET("/otc/volumetypes", listVolumeTypesHandler)
-}
-
-var (
-	ErrNoAuthURL  = fmt.Errorf("Environment variable OS_AUTH_URL needs to be set.")
-	ErrNoUsername = fmt.Errorf("Environment variable OS_USERNAME, OS_USERID, or OS_TOKEN needs to be set.")
-	ErrNoPassword = fmt.Errorf("Environment variable OS_PASSWORD or OS_TOKEN needs to be set.")
-)
-var nilOptions = gophercloud.AuthOptions{}
-
-// From https://github.com/gophercloud/gophercloud/blob/master/openstack/auth_env.go
-// Updated to use viper and fallback to env
-func authOptionsFromEnv() (gophercloud.AuthOptions, error) {
-	cfg := config.Config()
-
-	authURL := cfg.GetString("os_auth_url")
-	username := cfg.GetString("os_username")
-	userID := cfg.GetString("os_userid")
-	password := cfg.GetString("os_password")
-	tenantID := cfg.GetString("os_tenant_id")
-	tenantName := cfg.GetString("os_tenant_name")
-	domainID := cfg.GetString("os_domain_id")
-	domainName := cfg.GetString("os_domain_name")
-	applicationCredentialID := cfg.GetString("os_application_credential_id")
-	applicationCredentialName := cfg.GetString("os_application_credential_name")
-	applicationCredentialSecret := cfg.GetString("os_application_credential_secret")
-
-	// If OS_PROJECT_ID is set, overwrite tenantID with the value.
-	if v := cfg.GetString("os_project_id"); v != "" {
-		tenantID = v
-	}
-
-	// If OS_PROJECT_NAME is set, overwrite tenantName with the value.
-	if v := cfg.GetString("os_project_name"); v != "" {
-		tenantName = v
-	}
-
-	// end custom part
-
-	if authURL == "" {
-		err := gophercloud.ErrMissingEnvironmentVariable{
-			EnvironmentVariable: "OS_AUTH_URL",
-		}
-		return nilOptions, err
-	}
-
-	if userID == "" && username == "" {
-		// Empty username and userID could be ignored, when applicationCredentialID and applicationCredentialSecret are set
-		if applicationCredentialID == "" && applicationCredentialSecret == "" {
-			err := gophercloud.ErrMissingAnyoneOfEnvironmentVariables{
-				EnvironmentVariables: []string{"OS_USERID", "OS_USERNAME"},
-			}
-			return nilOptions, err
-		}
-	}
-
-	if password == "" && applicationCredentialID == "" && applicationCredentialName == "" {
-		err := gophercloud.ErrMissingEnvironmentVariable{
-			EnvironmentVariable: "OS_PASSWORD",
-		}
-		return nilOptions, err
-	}
-
-	if (applicationCredentialID != "" || applicationCredentialName != "") && applicationCredentialSecret == "" {
-		err := gophercloud.ErrMissingEnvironmentVariable{
-			EnvironmentVariable: "OS_APPLICATION_CREDENTIAL_SECRET",
-		}
-		return nilOptions, err
-	}
-
-	if applicationCredentialID == "" && applicationCredentialName != "" && applicationCredentialSecret != "" {
-		if userID == "" && username == "" {
-			return nilOptions, gophercloud.ErrMissingAnyoneOfEnvironmentVariables{
-				EnvironmentVariables: []string{"OS_USERID", "OS_USERNAME"},
-			}
-		}
-		if username != "" && domainID == "" && domainName == "" {
-			return nilOptions, gophercloud.ErrMissingAnyoneOfEnvironmentVariables{
-				EnvironmentVariables: []string{"OS_DOMAIN_ID", "OS_DOMAIN_NAME"},
-			}
-		}
-	}
-
-	ao := gophercloud.AuthOptions{
-		IdentityEndpoint:            authURL,
-		UserID:                      userID,
-		Username:                    username,
-		Password:                    password,
-		TenantID:                    tenantID,
-		TenantName:                  tenantName,
-		DomainID:                    domainID,
-		DomainName:                  domainName,
-		ApplicationCredentialID:     applicationCredentialID,
-		ApplicationCredentialName:   applicationCredentialName,
-		ApplicationCredentialSecret: applicationCredentialSecret,
-	}
-
-	return ao, nil
 }
 
 func getProvider() (*gophercloud.ProviderClient, error) {
