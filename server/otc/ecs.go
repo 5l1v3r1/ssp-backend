1907e59d4162fc03766aad5ae362d12463bb4be7
e507889 <lorenz.bischof@sbb.ch>
2019-09-10 13:55:18 +0200

    Remove old code

--- server/otc/ecs.go
+++ server/otc/ecs.go
@@ -5,12 +5,10 @@ import (
 	"github.com/SchweizerischeBundesbahnen/ssp-backend/server/common"
 	"github.com/SchweizerischeBundesbahnen/ssp-backend/server/config"
 	"github.com/gin-gonic/gin"
-	"github.com/gofrs/uuid"
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack/blockstorage/v1/volumetypes"
 	"github.com/gophercloud/gophercloud/openstack/blockstorage/v3/volumes"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/availabilityzones"
-	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/bootfromvolume"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/keypairs"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/extensions/startstop"
 	"github.com/gophercloud/gophercloud/openstack/compute/v2/flavors"
@@ -110,198 +108,6 @@ func validateUserInput(data NewECSCommand) error {
 	return nil
 }
 
-func newECSHandler(c *gin.Context) {
-	networkId := config.Config().GetString("otc_network_uuid")
-	if networkId == "" {
-		log.Println("Environment variable OTC_NETWORK_UUID must be set.")
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-		return
-	}
-
-	username := common.GetUserName(c)
-	log.Printf("%v creates new ECS @ OTC.", username)
-
-	var data NewECSCommand
-	err := c.BindJSON(&data)
-
-	if err != nil {
-		log.Println("Binding request to Go struct failed.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: wrongAPIUsageError})
-		return
-	}
-
-	err = validateUserInput(data)
-
-	if err != nil {
-		log.Println("User input validation failed.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: err.Error()})
-		return
-	}
-
-	client, err := getComputeClient()
-
-	if err != nil {
-		log.Println("Error getting compute client.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-		return
-	}
-
-	serverName, err := generateECSName(data.ECSName)
-
-	if err != nil {
-		log.Println("Error generating server name.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-		return
-	}
-
-	uniqueId, err := uuid.NewV4()
-	if err != nil {
-		log.Println("Error getting UUID. That's incredible.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-		return
-	}
-
-	blockDevices, err := createECSDisks(data, serverName, uniqueId.String(), username)
-	if err != nil {
-		log.Println("Error creating disks for ECS.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-		return
-	}
-
-	keyPair, err := createKeyPair(client, serverName+"-"+username+"-"+uniqueId.String(), data.PublicKey)
-	if err != nil {
-		log.Println("Error getting key.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-		return
-	}
-
-	userdata := []byte(fmt.Sprintf(`
-#cloud-config
-fqdn: %v
-hostname: %v
-manage_etc_hosts: false
-bootcmd:
-  - [ cloud-init-per, once, vgsystem, sh, -c, 'ret=1; i=0; while [[ $i -lt 120 ]]; do test -b /dev/vdb && lvm vgcreate vg_system /dev/vdb && ret=0 && break; i=$((i+5)); sleep 5; done; exit $ret' ]
-  - [ cloud-init-per, once, mkswap, sh, -c, 'lvm vgs vg_system && lvm lvcreate -nswap -L4G vg_system && mkswap /dev/vg_system/swap' ]
-  - [ cloud-init-per, once, mktmp, sh, -c, 'lvm vgs vg_system && lvm lvcreate -ntmp -L2G vg_system && mkfs.xfs /dev/vg_system/tmp && sed -i "$ a /dev/vg_system/tmp /tmp xfs nodev,nosuid 0 0" /etc/fstab && mount /tmp' ]
-  - [ cloud-init-per, once, mklog, sh, -c, 'lvm vgs vg_system && lvm lvcreate -nlog -L1G vg_system && mkfs.xfs /dev/vg_system/log && sed -i "$ a /dev/vg_system/log /var/log xfs nodev,nosuid,noexec 0 0" /etc/fstab && logdirs=$(find /var/log -mindepth 1 -maxdepth 1 -type d) && mount /var/log && mkdir $logdirs && mkdir /var/log/journal && ( type restorecon && restorecon -rv /var/log || true )' ]
-  - [ cloud-init-per, once, vgdata, sh, -c, 'ret=1; i=0; while [[ $i -lt 120 ]]; do test -b /dev/vdc && lvm vgcreate vg_data /dev/vdc && ret=0 && break; i=$((i+5)); sleep 5; done; exit $ret' ]
-  - [ cloud-init-per, once, mkhome, sh, -c, 'lvm vgs vg_data && lvm lvcreate -nhome -L4G vg_data && mkfs.xfs /dev/vg_data/home && sed -i "$ a /dev/vg_data/home /home xfs nodev,nosuid 0 0" /etc/fstab && mount /home' ]
-runcmd:
-  - [ cloud-init-per, once, chownhome, sh, -c, 'chown -R 1000:1000 /home/*' ]`, serverName, serverName))
-
-	serverCreateOpts := servers.CreateOpts{
-		Name:             serverName,
-		AvailabilityZone: data.AvailabilityZone,
-		FlavorRef:        data.FlavorName,
-		UserData:         userdata,
-		Networks: []servers.Network{
-			{
-				UUID: networkId,
-			},
-		},
-		Metadata: map[string]string{
-			"Owner":   username,
-			"Billing": data.Billing,
-			"Mega ID": data.MegaId,
-		},
-	}
-
-	keyCreateOpts := keypairs.CreateOptsExt{
-		CreateOptsBuilder: serverCreateOpts,
-		KeyName:           keyPair.Name,
-	}
-
-	_, err = bootfromvolume.Create(client, bootfromvolume.CreateOptsExt{
-		CreateOptsBuilder: keyCreateOpts,
-		BlockDevice:       blockDevices,
-	}).Extract()
-
-	if err != nil {
-		log.Println("Creating server failed.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: "Server konnte nicht erstellt werden."})
-		return
-	} else {
-		log.Println("Creating server succeeded.")
-		c.JSON(http.StatusOK, common.ApiResponse{Message: "Server erstellt."})
-		return
-	}
-}
-
-func createECSDisks(data NewECSCommand, serverName string, uniqueId string, username string) ([]bootfromvolume.BlockDevice, error) {
-	log.Println("Creating root, system and data disk.")
-
-	blockStorageClient, err := getBlockStorageClient()
-	if err != nil {
-		log.Println("Error getting block storage client.", err.Error())
-		return nil, err
-	}
-
-	metadata := map[string]string{
-		"Owner":   username,
-		"Billing": data.Billing,
-		"Mega ID": data.MegaId,
-	}
-
-	var blockDevices []bootfromvolume.BlockDevice
-
-	// create root disk volume
-	volOpts := []volumes.CreateOpts{
-		{
-			Name:             serverName + "-" + uniqueId,
-			Description:      serverName + "-" + uniqueId,
-			Size:             data.RootDiskSize,
-			VolumeType:       data.RootVolumeTypeId,
-			ImageID:          data.ImageId,
-			AvailabilityZone: data.AvailabilityZone,
-			Metadata:         metadata,
-		},
-		{
-			Name:             serverName + "-" + uniqueId,
-			Description:      serverName + "-" + uniqueId,
-			Size:             data.SystemDiskSize,
-			VolumeType:       data.SystemVolumeTypeId,
-			AvailabilityZone: data.AvailabilityZone,
-			Metadata:         metadata,
-		},
-		{
-			Name:             serverName + "-" + uniqueId,
-			Description:      serverName + "-" + uniqueId,
-			Size:             data.DataDiskSize,
-			VolumeType:       data.DataVolumeTypeId,
-			AvailabilityZone: data.AvailabilityZone,
-			Metadata:         metadata,
-		},
-	}
-
-	for _, volOpt := range volOpts {
-		vol, err := volumes.Create(blockStorageClient, volOpt).Extract()
-		if err != nil {
-			log.Println("Creating volume failed.", err.Error())
-			return nil, err
-		}
-
-		// bootIndex should be 0 for root volume
-		var bootIndex int
-		if volOpt.ImageID == "" {
-			bootIndex = -1
-		}
-
-		blockDevices = append(blockDevices, bootfromvolume.BlockDevice{
-			SourceType:      bootfromvolume.SourceVolume,
-			DestinationType: bootfromvolume.DestinationVolume,
-			UUID:            vol.ID,
-			BootIndex:       bootIndex,
-		})
-
-		err = volumes.WaitForStatus(blockStorageClient, vol.ID, "available", 300)
-		if err != nil {
-			log.Println("Error while waiting on volume.", err.Error())
-			return nil, err
-		}
-	}
-	return blockDevices, nil
-}
 
 func listECSHandler(c *gin.Context) {
 	username := common.GetUserName(c)
@@ -552,41 +358,6 @@ func rebootECSHandler(c *gin.Context) {
 	return
 }
 
-func deleteECSHandler(c *gin.Context) {
-	log.Println("Deleting ECS @ OTC.")
-
-	client, err := getComputeClient()
-
-	if err != nil {
-		log.Println("Error getting compute client.", err.Error())
-		if err != nil {
-			c.JSON(http.StatusBadRequest, common.ApiResponse{Message: genericOTCAPIError})
-			return
-		}
-	}
-
-	var data ECServerListResponse
-	err = c.BindJSON(&data)
-
-	if err != nil {
-		log.Println("Binding request to Go struct failed.", err.Error())
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: wrongAPIUsageError})
-		return
-	}
-
-	for _, server := range data.ECServers {
-		deleteResult := servers.Delete(client, server.Id)
-
-		if deleteResult.Err != nil {
-			log.Println("Error while deleting server.", err.Error())
-			c.JSON(http.StatusBadRequest, common.ApiResponse{Message: "Mindestens ein server konnte nicht gelöscht werden."})
-			return
-		}
-	}
-
-	c.JSON(http.StatusOK, common.ApiResponse{Message: "Löschung wurde initiert."})
-	return
-}
 
 func createKeyPair(client *gophercloud.ServiceClient, publicKeyName string, publicKey string) (*keypairs.KeyPair, error) {
 	log.Printf("Creating public key with name %v.", publicKeyName)
@@ -858,17 +629,4 @@ func getImages(client *gophercloud.ServiceClient) (*ImageListResponse, error) {
 	}
 
 	return &result, nil
-}
-
-func generateECSName(ecsName string) (string, error) {
-	log.Println("Generating ECS name.")
-
-	// <prefix>-<ecsName>
-	ecsPrefix := config.Config().GetString("otc_ecs_prefix")
-
-	if len(ecsPrefix) < 1 {
-		return "", errors.New("Environment variable OTC_ECS_PREFIX must be set.")
-	}
-
-	return strings.ToLower(ecsPrefix + "-" + ecsName), nil
 }
