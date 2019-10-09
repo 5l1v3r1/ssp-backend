d3c826ba35478fd70f5f48c150068b369695ecf4
Benji <benjihaegler@gmail.com>
2019-10-09 08:58:46 +0200

    small change

--- server/common/apimodels.go
+++ server/common/apimodels.go
@@ -3,8 +3,7 @@ package common
 import "time"
 import "github.com/aws/aws-sdk-go/service/ec2"
 
-const ConfigNotSetError = "Funktion deaktiviert oder falsch konfiguriert. Kontaktieren Sie bitte das CLP Team"
-"Function deactivated or wrong configurated. Please contact the CLP Team"
+const ConfigNotSetError = "Function deactivated or wrong configurated. Please contact the CLP Team"
 
 type ProjectName struct {
 	Project string `json:"project"`
