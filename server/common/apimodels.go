d05947a70289051035a161facb898ee37aebd1a7
lbischof <1837725+lbischof@users.noreply.github.com>
2020-02-12 15:06:21 +0100

    Revert "Release of ssp with openshift4 support"

--- server/common/apimodels.go
+++ server/common/apimodels.go
@@ -217,9 +217,3 @@ type JsonPatch struct {
 	Path      string      `json:"path"`
 	Value     interface{} `json:"value"`
 }
-
-type OpenshiftUser struct {
-	ApiGroup string `json:"apiGroup"`
-	Kind     string `json:"kind"`
-	Name     string `json:"name"`
-}
