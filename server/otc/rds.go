f3c1ef902d4c26de759b825b1b8693d661251d77
e507889 <lorenz.bischof@sbb.ch>
2020-03-16 13:16:36 +0100

    Update error message

--- server/otc/rds.go
+++ server/otc/rds.go
@@ -59,7 +59,7 @@ func listRDSFlavorsHandler(c *gin.Context) {
 func listRDSVersionsHandler(c *gin.Context) {
 	stage := c.Request.URL.Query().Get("stage")
 	if stage != "p" && stage != "t" {
-		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: "Wrong API usage. Missing parameter stage"})
+		c.JSON(http.StatusBadRequest, common.ApiResponse{Message: "Wrong API usage. Parameter stage is: %v. Should be p or t", stage})
 		return
 	}
 	tenant := fmt.Sprintf("SBB_RZ_%v_001", strings.ToUpper(stage))
